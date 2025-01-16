fc8bb576f4854cf6ceca629091b08113119c4501
YYH123123813 <eaau6503@gmail.com>
2025-01-16 17:07:42 +0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("myButton");
+    button.addEventListener("click", function() {
+        alert("按钮点击了！");
+    });
+});
